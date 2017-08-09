919e4368bb7ff62ccaa515d4d09c60f7749c46be
Amber Baughman <pyrephox@gmail.com>
2017-08-09 11:11:13 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Goal of this assignment is to demonstrate lexical scoping and to create a pair
+## of functions (makeCacheMatrix) and (cacheSolve), which will allow you to compute
+## and store the inverse of a matrix, so that if the inverse already exists, it does
+## not need to be recomputed, but can simply be looked up in the cache. 
 
-## Write a short comment describing this function
+## This function will create a matrix object which can cache the input's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  store_inv <- NULL
+  set <- function(y) {
+    x <<- y
+    store_inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) store_inv <<- inverse
+  getinv <- function() store_inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function will check to see if the inverse of the matrix object created above
+## has already been calculated. If it has, it will pull from the cache. If not,
+## it will calculate the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    print("Cached result found. Retrieving.")
+    return(inv)
+  }
+  data <-x$get()
+  inv <- solve(data)
+  x$setinv(inv)
+  inv ## Return a matrix that is the inverse of 'x'
 }
